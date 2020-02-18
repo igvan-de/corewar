8a14087294a31ce7c68862f43ee00a9d73064dcf
Jesse Dunnink <jdunnink@f1r6s13.codam.nl>
2020-02-18 09:55:37 +0100

    added comments to load_optab

--- vm/vm_srcs/main.c
+++ vm/vm_srcs/main.c
@@ -12,8 +12,26 @@
 
 #include "vm.h"
 
+static	void	print_op_name(t_env *env)
+{
+	int i;
+	t_op curr_op;
+
+	i = 0;
+	while (i < 16)
+	{
+		curr_op = env->op_tab[i];
+		ft_putendl(curr_op.name);
+		i++;
+	}
+}
+
 int main(void)
 {
+	t_env env;
 	ft_putstr("Welcome to Corewar!\n");
+	
+	load_optab(&env);
+	print_op_name(&env);
 	return (0);
 }
