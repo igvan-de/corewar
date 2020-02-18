22ea7eb6f2988eb6f899674a7df4b52758d0c064
Jesse Dunnink <jdunnink@f1r6s13.codam.nl>
2020-02-18 09:28:08 +0100

    includes operations header (op.h) in includes foler and c-file (load_optab.c) with a load function attached. defined a global environment struct in vm.h and added load_optab function prototype. moved vm c_files into a srcs directory

--- includes/vm.h
+++ includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: jdunnink <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/18 08:47:01 by jdunnink      #+#    #+#                 */
-/*   Updated: 2020/02/18 08:48:12 by jdunnink      ########   odam.nl         */
+/*   Updated: 2020/02/18 09:02:29 by jdunnink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 # define VM_H
 
 #include "libft.h"
+#include "op.h"
+
+/*
+**	global environment struct containg main reference data and ptrs (initialized once)
+*/
+
+typedef struct	s_env
+{
+	t_op op_tab[17];
+}				t_env;
+
+void	load_optab(t_env *env);
 
 #endif
