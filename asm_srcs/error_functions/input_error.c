e2ea2ba63c19c233759e0618a6881904cdb2798c
Mark Lokhorst <mlokhors@student.codam.nl>
2020-02-20 13:51:00 +0100

    playing around to have a design on the structure. can be wiped if needed

--- asm_srcs/error_functions/input_error.c
+++ asm_srcs/error_functions/input_error.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   error_functions.c                                  :+:    :+:            */
+/*   input_error.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/17 16:58:13 by igvan-de       #+#    #+#                */
-/*   Updated: 2020/02/17 17:09:17 by igvan-de      ########   odam.nl         */
+/*   Updated: 2020/02/20 13:47:52 by mlokhors      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,23 @@
 void	input_error(void)
 {
 	ft_putendl("No champion is give, please provide champion");
+	exit(-1);
+}
+
+void	input_not_asm(void)
+{
+	ft_putendl("This file is not am assembly file");
+	exit(-1);
+}
+
+void	input_bad_asm(void)
+{
+	ft_putendl("This file will not generate a working .cor file");
+	exit(-1);
+}
+
+void	failed_malloc_process(void)
+{
+	ft_putendl("Malloc failed in process asm function");
 	exit(-1);
 }
--- asm_srcs/main.c
+++ asm_srcs/main.c
@@ -6,21 +6,81 @@
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/17 15:46:14 by igvan-de       #+#    #+#                */
-/*   Updated: 2020/02/18 11:45:24 by igvan-de      ########   odam.nl         */
+/*   Updated: 2020/02/20 13:47:52 by mlokhors      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
+// 1 file of meerdere
 
+
+char	*ft_strndup(const char *s1, int leng)
+{
+	int		i;
+	int		length;
+	char	*dest;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	if (i > length)
+		i = length;
+	dest = (char*)ft_memalloc(sizeof(char) * i + 1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] && i < leng)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*trim_name(char *file_name)
+{
+	int length;
+
+	length = ft_strlen(file_name);
+	length -= 2;
+	return(ft_strndup(file_name, length));
+}
+
+void	process_asm(char *file_name)
+{
+	int		fd;
+	char	*cor_name;
+	int 	len;
+	
+	cor_name = trim_name(file_name);
+	if (!cor_name)
+		failed_malloc_process();
+	fd = ();
+}
+/*
+void	check_file(char *file_name, int *name_len)
+{
+	if (check_correct_file(file_name, name_len) == 1)
+		input_not_asm();
+	if (check_asm_file(file_name) == 1)
+		input_bad_asm();
+}
+*/
 int	main(int argc, char **argv)
 {
 	int	i;
+	int name_len;
 
+	name_len = 0;
 	i = 1;
 	if (argc == 0)
 		input_error();
 	while (argv[i])
 	{
+		name_len = 0;
+//		check_file(argv[i], &name_len);
+		process_asm(argv[i]);
 		i++;
 	}
 	return (0);
