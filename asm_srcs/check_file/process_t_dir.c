589cce8d2d940d41aa035caf91ebbfcab61dca4f
warhamer <mlokhors@student.codam.nl>
2020-04-14 23:05:49 +0200

    t_dir file changed a condition so it also checks end of string

--- asm_srcs/check_file/process_t_dir.c
+++ asm_srcs/check_file/process_t_dir.c
@@ -6,7 +6,7 @@
 /*   By: mark <mark@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/03 01:13:15 by mark          #+#    #+#                 */
-/*   Updated: 2020/04/09 03:04:05 by mark          ########   odam.nl         */
+/*   Updated: 2020/04/14 23:02:35 by mark          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ void		get_label_op(t_func_list *list, t_direction *new, int arg)
 	i = list->line_char + 1;
 	while (check_label_char(list->line[i]) == true)
 		i++;
-	if (ft_isspace(list->line[i]) == 0 && list->line[i] != SEPARATOR_CHAR)
+	if (ft_isspace(list->line[i]) == 0 && (list->line[i] != SEPARATOR_CHAR &&
+	list->line[i] != '\0'))
 		error_message(list, 111, 0, 11);
 	i--;
 	new->arg_str[arg] = ft_strsub(list->line, list->line_char + 1, i - list->line_char);
@@ -44,10 +45,12 @@ void		process_t_dir(t_func_list *list, t_direction *new, int arg)
 	list->total_bytes += dir_size;
 	insert_encode(new, arg, DIR_CODE);
 	list->line_char++;
+	ft_printf("testing char %c with number %d\n", list->line[list->line_char], list->line_char);
 	if (list->line[list->line_char] == LABEL_CHAR)
 	{
 		new->label_in_op = 1;
 		get_label_op(list, new, arg);
+		return ;
 	}
 	else if (!((list->line[list->line_char] >= '0' && list->line[list->line_char] <= '9') ||
 	list->line[list->line_char] == '-'))
