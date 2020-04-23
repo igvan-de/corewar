d922e3c7bebeb61e442da72e0f032f247a82857b
jesse <dunninkjesse1@gmail.com>
2020-04-23 14:15:53 +0200

    fixed bug where commets after instructions would cause an input_error

--- asm_srcs/check_file/process_t_reg.c
+++ asm_srcs/check_file/process_t_reg.c
@@ -28,12 +28,8 @@ void		process_t_reg(t_func_list *list, t_direction *new, int arg)
 		list->line[list->line_char] <= '9'))
 		error_message(list, 99, 0, 9);
 	converted = pm_atoi(list);
-	if (converted <= 0 || converted > 16)
+	if (converted <= 0 || converted > REG_NUMBER)
 		error_message(list, 90, 1, 9);
-	if (list->line[list->line_char] != SEPARATOR_CHAR &&
-		list->line[list->line_char] != '\0' &&
-		ft_isspace(list->line[list->line_char]) == 1)
-		error_message(list, 98, 0, 9);
 	insert_encode(new, arg, REG_CODE);
 	new->arg_num[arg] = converted;
 }
