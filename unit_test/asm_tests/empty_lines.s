f2360a3f82e55ae2ed6879234f62980d72afe5e4
jesse <dunninkjesse1@gmail.com>
2020-04-30 12:17:33 +0200

    additional test files for asm

---
+++ unit_test/asm_tests/empty_lines.s
@@ -0,0 +1,20 @@
+.name       "empty_lines"
+	.comment    "multiple empty lines"
+
+	ld	%50, r2
+	ld	%100, r3	
+	add r2, r3, r4
+
+	add r56, r45, r99
+	zjmp	%:sti	
+sti:
+	sti r99, %23, %2834
+	zjmp	%:end
+sti:
+	sti r2, %23, %2834
+
+	zjmp	%:end
+sti:
+	sti r3, %23, %2834
+	zjmp	%:end
+end:
