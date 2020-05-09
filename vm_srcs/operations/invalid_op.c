ac46bc5da3b9921b52e981b8584bf1cbc7296530
jesse <dunninkjesse1@gmail.com>
2020-05-09 14:00:38 +0200

    fixed norme error

--- vm_srcs/operations/invalid_op.c
+++ vm_srcs/operations/invalid_op.c
@@ -75,6 +75,16 @@ static	void		walk_one(t_cursor *c, t_env *env, t_byt *bytes, unsigned *i)
 	}
 }
 
+static	void		search_op(t_env *env, int max_bytes, int *bytes, int *i)
+{
+	while ((env->map[modi(*i)] < 1 || 16 < env->map[modi(*i)]) &&
+			*bytes < max_bytes)
+	{
+		(*i)++;
+		(*bytes)++;
+	}
+}
+
 /*
 **	@brief:	move cursor past invalid operation code
 **
@@ -100,12 +110,7 @@ void				invalid_op(t_cursor *cursor, t_env *env, int type)
 	bytes = 1;
 	index = modi(cursor->position + 1);
 	encode = env->map[modi(cursor->position + 1)];
-	while ((env->map[modi(index)] < 1 || 16 < env->map[modi(index)]) &&
-			bytes < max_bytes)
-	{
-		index++;
-		bytes++;
-	}
+	search_op(env, max_bytes, &bytes, &index);
 	bytes = validate_jump(cursor->op_code, type, bytes, encode);
 	index = modi(cursor->position + bytes);
 	walk_one(cursor, env, &bytes, &index);
